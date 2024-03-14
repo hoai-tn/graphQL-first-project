e816190794e545b74918b74ecdd7ff1b74ad5dc0
Hoai Tran <tranngochoai02051999@gmail.com>
2024-03-14 17:26:38 +0700

    [TASK] Install tailwind

--- graphql-client/src/App.vue
+++ graphql-client/src/App.vue
@@ -1,47 +1,10 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
+<script setup lang="ts"></script>
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ul class="steps">
+    <li class="step step-primary">Register</li>
+    <li class="step step-primary">Choose plan</li>  
+    <li class="step">Purchase</li>
+    <li class="step">Receive Product</li>
+  </ul>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
